6057338444abff686d598440a7ba905b604d88c3
Mário Pinto <mlmtpinto@gmail.com>
2025-06-08 15:25:30 +0100

    Implemented partially RabbitMQ

--- src/Interface/ObserverRI.java
+++
@@ -1,9 +0,0 @@
-package Interface;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ObserverRI extends Remote {
-    public void update() throws RemoteException;
-}
-
--- src/Interface/ReplicationInterface.java
+++
@@ -1,9 +0,0 @@
-package Interface;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.Map;
-
-public interface ReplicationInterface extends Remote {
-    void setFullState(Map<String, Object> state) throws RemoteException;
-}
